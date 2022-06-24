435b276408f19183c519ba611428541d780f6160
Oscar Santos <oscarsantosmu@gmail.com>
2022-06-24 03:32:50 +0200

    Add: terraform to create storage account

---
+++ src/iac/azure/create_storage_account/variables.tf
@@ -0,0 +1,16 @@
+# Input variable: Name of Resource Group
+variable "resource_group_name" {
+  default = "tfstorageaccounteduenvs-rg"
+}
+
+# Input variable: Name of Storage Account
+variable "storage_account_name" {
+  description = "The name of the storage account. Must be globally unique, length between 3 and 24 characters and contain numbers and lowercase letters only."
+  default     = "tfstorageaccounteduenvs"
+}
+
+# Input variable: Name of Storage container
+variable "container_name" {
+  description = "The name of the Blob Storage container."
+  default     = "terraform-state-container"
+}
