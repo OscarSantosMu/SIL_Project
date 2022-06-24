ba8454468c713d3e3f2201bc25af26a6b1e467c4
Oscar Santos <oscarsantosmu@gmail.com>
2022-06-24 03:31:21 +0200

    Add: terraform state to storage account

---
+++ src/iac/azure/backup/main.tf
@@ -0,0 +1,16 @@
+# Set the Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+# Configure the Microsoft Azure provider
+provider "azurerm" {
+  features {}
+}
